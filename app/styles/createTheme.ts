9e56688b068ff2f8240f6aefbdcfcf3e031fb895
Yaniv Mordekhay <yaniv.mordekhay@firstdag.com>
2019-04-29 11:07:46 +0300

    Theme management MUI-style
    The old styling code should be removed. Will do once the dust settles.

---
+++ app/styles/createTheme.ts
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import createMuiTheme, {
+  ThemeOptions,
+} from '@material-ui/core/styles/createMuiTheme';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
+
+/**
+ * Augment createMuiTheme to add additional theming for the app drawer.
+ */
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Theme {
+    navDrawer: {
+      width: React.CSSProperties['width'];
+      breakpoint: Breakpoint;
+    };
+  }
+  interface ThemeOptions {
+    navDrawer?: {
+      width?: React.CSSProperties['width'];
+      breakpoint?: Breakpoint;
+    };
+  }
+}
+
+/**
+ * Creates the default theme with our own additions.
+ */
+export default function createTheme(options: ThemeOptions) {
+  return createMuiTheme({
+    navDrawer: {
+      width: 240,
+      breakpoint: 'lg',
+    },
+    ...options,
+  });
+}
